32f69226922c206093d4459422551f22411337fa
Casey Maltese <caseymaltese@gmail.com>
2020-10-03 13:16:06 -0400

    code finalized

--- config/connection.js
+++ config/connection.js
@@ -19,7 +19,7 @@ if (process.env.JAWSDB_URL) {
     password: "root",
     database: "burgers_db"
 });
-
+}
 connection.connect(function(err) {
     if (err) {
       console.error("error connecting: " + err.stack);
